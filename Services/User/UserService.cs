42bc76467d91471e26abb853050b9f53d78969c2
Wesley254 <wesleykirui2021@gmail.com>
2023-09-22 18:42:46 +0300

    User Service

--- Services/User/UserService.cs
+++ Services/User/UserService.cs
@@ -2,10 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TwitterClone.Models;
 
 namespace TwitterClone.Services.User
 {
-    public class UserService
+    public class UserService : IUser
     {
+        private readonly HttpClient _httpClient;
+
+        private readonly string _baseUrl = "https://jsonplaceholder.typicode.com/users";
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<Users>> GetUsers()
+        {
+            var res = await _httpClient.GetAsync(_baseUrl);
+            var response = await res.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<Users>>(response.ToString());
+            if (result != null)
+            {
+                return result;
+            }
+            return new List<Users>();
+        }
     }
 }
